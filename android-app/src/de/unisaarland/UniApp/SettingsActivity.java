0cc471430a7746c533e5b9e41d5a1d8a005de293
Clemens Hammacher <hammacher@cs.uni-saarland.de>
2015-10-29 17:03:51 +0100

    Update the campus selection view on pref change

--- android-app/src/de/unisaarland/UniApp/SettingsActivity.java
+++ android-app/src/de/unisaarland/UniApp/SettingsActivity.java
@@ -1,6 +1,9 @@
 package de.unisaarland.UniApp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
@@ -21,13 +24,55 @@ public class SettingsActivity extends ActionBarActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-    public static class SettingsFragment extends PreferenceFragment {
+    // Just setting the summary to "%s" does not work, as older android versions do not update
+    // the label when the preference value changes (cmp.
+    // http://stackoverflow.com/questions/7017082/change-the-summary-of-a-listpreference-with-the-new-value-android/15329652#15329652)
+    // So we just add a new pref change listener and update the campus selection view ourselves.
+    public static class SettingsFragment extends PreferenceFragment
+            implements SharedPreferences.OnSharedPreferenceChangeListener {
+
         @Override
         public void onCreate(Bundle paramBundle) {
             super.onCreate(paramBundle);
             addPreferencesFromResource(R.xml.preferences);
             getActivity().setTheme(R.style.AppTheme);
         }
-    }
 
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            Preference pref0 = findPreference(key);
+            if (!(pref0 instanceof ListPreference))
+                return;
+            ListPreference pref = (ListPreference) pref0;
+            String value = pref.getValue();
+            CharSequence[] entries = pref.getEntries();
+            CharSequence[] entryValues = pref.getEntryValues();
+            CharSequence summ = pref.getSummary();
+            // check whether the current summary matches any entry. if so, update it with
+            // the new entry.
+            for (CharSequence cs : entries) {
+                if (summ.toString().equals(cs.toString())) {
+                    for (int i = 0; i < entryValues.length; ++i) {
+                        if (entryValues[i].toString().equals(value)) {
+                            pref.setSummary(entries[i]);
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+    }
 }
